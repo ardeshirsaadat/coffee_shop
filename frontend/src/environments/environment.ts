48c16714c784db447b5659a5eacbc3b4716cbb9f
ardeshir <49168102+ardeshirsaadat@users.noreply.github.com>
2021-02-04 10:29:26 -0500

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   auth0: {
     url: 'coffee-shop-ardeshir.us', // the auth0 domain prefix
     audience: 'drinks', // the audience set for the auth0 app
-    clientId: 'tZSdNvx7zxCON5QmLI7qr64zOoXnXeCa', // the client id generated for the auth0 app
+    clientId: '', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
